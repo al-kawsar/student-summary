ec65bb2f7ebf3912885dd214b4e597a0cd0acfbe
andirhn <tlod9906@gmail.com>
2024-02-23 17:57:20 -1000

    feat: set up route api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\GuruController;
+use App\Http\Controllers\Api\InformasiController;
+use App\Http\Controllers\Api\SiswaController;
+use App\Http\Controllers\Api\User;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +22,51 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::controller(SiswaController::class)
+    ->prefix('students')
+    ->group(function () {
+        Route::get('/', 'getData');
+        Route::get('/{siswa:username}', 'getDetailSiswa');
+        Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+            Route::post('/', 'store');
+            Route::put('/{siswa:uuid}', 'edit');
+            Route::delete('/{siswa:uuid}', 'delete');
+        });
+    });
+
+Route::controller(GuruController::class)
+    ->prefix('teachers')
+    ->group(function () {
+        Route::get('/', 'getData');
+        Route::get('/{guru:username}', 'getDetailGuru');
+        Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+            Route::post('/', 'store');
+            Route::put('/{guru:uuid}', 'edit');
+            Route::delete('/{guru:uuid}', 'delete');
+        });
+    });
+
+Route::controller(InformasiController::class)
+    ->prefix('announcements')
+    ->group(function () {
+        Route::get('/', 'getData');
+        Route::get('/{slug}', 'getDetail');
+        Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+            Route::post('/', 'store');
+            Route::put('/{slug}', 'edit');
+            Route::delete('/{slug}', 'delete');
+        });
+    });
+
+Route::view('/about', 'pages.about')->name('about.index');
+
+Route::name('auth.')->prefix('auth')->middleware(['guest'])->group(function(){
+    Route::get('register', 'registerPage')->name('register.index');
+    Route::post('register', 'register')->name('register');
+    Route::get('login', 'loginPage')->name('login.index');
+    Route::post('login', 'login')->name('login');
+    Route::get('logout', 'logout')->name('logout')->withoutMiddleware(['guest'])->middleware(['auth']);
+});
+
+// Route::middleware(['auth',])
